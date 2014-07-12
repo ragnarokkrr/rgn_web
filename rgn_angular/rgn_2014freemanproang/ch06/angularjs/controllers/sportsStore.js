7ae5e78bc892437d5a3378ef0ce9bb83bb775c63
Nilseu Padilha <ragnarokkrr@gmail.com>
2014-07-12 04:11:13 -0300

    chap 6

---
+++ rgn_angular/rgn_2014freemanproang/ch06/angularjs/controllers/sportsStore.js
@@ -0,0 +1,47 @@
+/**
+ * Top Level Angular JS Controller for application
+ */
+angular.module("sportsStore")
+	.constant("dataUrl", "http://localhost:5500/products")
+	.constant("orderUrl", "http://localhost:5500/orders")
+	.controller("sportsStoreCtrl", function($scope, $http, $location, 
+			dataUrl, orderUrl, cart){
+		//binds array to sportsStoreCtrl $scope
+//		$scope.data = {
+//			products: [
+//			{name: "Product #1", description: "A Product", category: "Category #1", price:100}
+//			,{name: "Product #2", description: "A Product", category: "Category #1", price:110}
+//			,{name: "Product #3", description: "A Product", category: "Category #2", price:210}
+//			,{name: "Product #4", description: "A Product", category: "Category #3", price:202}
+//		]};
+
+		$scope.data = {};
+		
+		$http.get(dataUrl)
+			.success(function(data){
+				$scope.data.products = data;
+			})
+			.error(function(error){
+				$scope.data.error = error;
+			});
+		
+		
+		$scope.sendOrder = function(shippingDetails){
+			var order = angular.copy(shippingDetails);
+			order.products = cart.getProducts();
+			
+			$http.post(orderUrl, order)
+				.success(function(data){
+					$scope.data.orderId = data.id;
+					cart.getProducts().length = 0;
+				})
+				.error(function (error){
+					$scope.data.orderError = error;
+				})
+				.then(function(){
+					$location.path("complete");
+				});
+			
+		};
+		
+	});
